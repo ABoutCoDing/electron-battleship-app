7f2be93070f7b26b890844619641cf08c58c2b0f
jamie <jamie@snowkiwi.com>
2017-03-30 20:43:35 +0900

    commit

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ const {app, BrowserWindow, ipcMain, Menu} = require('electron')
 const path = require('path')
 const url = require('url')
 const fs = require('fs')
-//const template = [
-//  {
-//    label: '편집',
-//    submenu: [
-//      {
-//        label: '스크린샷',
-//        click () { capture() }
-//      }
-//    ]
-//  }
-//]
+const template = [
+  {
+    label: '편집',
+    submenu: [
+      {
+        label: '스크린샷',
+        click () { capture() }
+      }
+    ]
+  }
+]
 
 let win
 
@@ -26,23 +26,23 @@ function createWindow () {
     slashes: true
   }))
 
-//  if (process.platform === 'darwin') {  // NeXTSTEP, BSD, Mach, OSX 이라면
-//    template.unshift({  // 새요소 추가
-//      label: app.getName(),
-//      submenu: [
-//        {
-//          role: 'about'
-//        },
-//        {
-//          label: 'ABCD(ABoutCoDing) Homepage',
-//          click () { require('electron').shell.openExternal('http://abcds.kr') }
-//        }
-//       ]
-//    })
-//  }
-//
-//  const menu = Menu.buildFromTemplate(template)
-//  Menu.setApplicationMenu(menu)
+  if (process.platform === 'darwin') {  // NeXTSTEP, BSD, Mach, OSX 이라면
+    template.unshift({  // 새요소 추가
+      label: app.getName(),
+      submenu: [
+        {
+          role: 'about'
+        },
+        {
+          label: 'ABCD(ABoutCoDing) Homepage',
+          click () { require('electron').shell.openExternal('http://abcds.kr') }
+        }
+       ]
+    })
+  }
+
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
 
   //  win.webContents.openDevTools();
 
@@ -50,22 +50,22 @@ function createWindow () {
     win = null
   })
 
-//  ipcMain.on('captureSync', function(event){
-//    capture()
-//    event.returnValue = '스크린샷 저장'
-//  })
+  ipcMain.on('captureSync', function(event){
+    capture()
+    event.returnValue = '스크린샷 저장'
+  })
 }
 
-//function capture() {
-//  win.capturePage(function(image) {
-//    console.log(getUserHome()+'/Documents/screenshot.png');
-//    fs.writeFileSync(getUserHome() + '/Documents/screenshot.png', image.toPng())
-//  });
-//}
-//
-//function getUserHome() {
-//  return process.env.HOME || process.env.USERPROFILE
-//}
+function capture() {
+  win.capturePage(function(image) {
+    console.log(getUserHome()+'/Documents/screenshot.png');
+    fs.writeFileSync(getUserHome() + '/Documents/screenshot.png', image.toPng())
+  });
+}
+
+function getUserHome() {
+  return process.env.HOME || process.env.USERPROFILE
+}
 
 app.on('ready', createWindow)
 
